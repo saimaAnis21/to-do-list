23c337e9fbc427b0358194cbf567e25358b62f20
saimaAnis21 <saima_rehman21@yahoo.com>
2021-04-25 12:30:50 +0500

    Fix the delete and display functionality

--- src/todoObj.js
+++ src/todoObj.js
@@ -1,42 +1,5 @@
-class toDo{
-    constructor(title,desc,ddate,priority) {
-        this.title = title;
-        this.desc = desc;
-        this .ddate= ddate;
-        this.priority = priority;
-    }
-
-    get title() {
-        return this._title;
-      }
-    
-      get desc() {
-        return this._desc;
-      }
-    
-      get ddate() {
-        return this._ddate;
-      }
-    
-      get priority() {
-        return this._priority;
-      }
-
-      set title(val) {
-        this._title = val;
-      }
-    
-      set desc(val) {
-        this._desc = val;
-      }
-    
-      set ddate(val) {
-        this._ddate = val;
-      }
-    
-      set priority(val) {
-        this._priority = val;
-      }
-}
+const toDo = (title, desc, ddate, priority) => ({
+    title, desc, ddate, priority,
+});
 
 export default toDo;
